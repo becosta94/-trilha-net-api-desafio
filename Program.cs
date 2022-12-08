4544322964cf6b7c5f3daca065959a0fe341ed1c
Bernardo Lopes <bernardo.caetano94@gmail.com>
2022-12-08 12:29:31 -0300

    Finalização desafio

---
+++ Program.cs
@@ -0,0 +1,38 @@
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+using TrilhaApiDesafio.Context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<OrganizadorContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<OrganizadorContext>();
+
+builder.Services.AddDbContext<OrganizadorContext>(options =>
+    options.EnableSensitiveDataLogging(true));
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
